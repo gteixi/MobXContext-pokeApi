31fac85c0d25cf22f066062c0290f8010aa17ed8
gteixi <gemmateixido25@gmail.com>
2021-11-26 10:59:46 +0100

    Fix button position

--- src/components/BackButton/BackButton.styles.ts
+++ src/components/BackButton/BackButton.styles.ts
@@ -2,18 +2,19 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     buttonContainer: {
-      width: 100,
-      height: 60,
-      paddingLeft: 90,
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'row',
+      margin: 10,
     },
     button: {
-      marginTop: 15,
-      marginRight: -80,
+      marginLeft: 120,
+      marginRight: 120,
+      height: 30,
       alignItems: 'center',
       justifyContent: 'center',
-      paddingVertical: 12,
       borderRadius: 4,
-      elevation: 3,
       backgroundColor: 'red',
       flex: 1,
     },
