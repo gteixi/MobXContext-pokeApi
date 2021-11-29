6a6b229271d3eed610b1ba384b5f76c407fa6d5a
gteixi <gemmateixido25@gmail.com>
2021-11-29 12:51:03 +0100

    Correct review of navigation in folder located at source and remove screenOptions

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Home from "../../screens/Home/Home"
-import PokemonDetails from "../../screens/PokemonDetails/PokemonDetails";
-import NextPokemons from "../../screens/NextPokemons/NextPokemons";
-import PokemonsNotFound from "../../screens/PokemonsNotFound/PokemonsNotFound";
+import Home from "../screens/Home/Home"
+import PokemonDetails from "../screens/PokemonDetails/PokemonDetails";
+import NextPokemons from "../screens/NextPokemons/NextPokemons";
+import PokemonsNotFound from "../screens/PokemonsNotFound/PokemonsNotFound";
 
 // REVIEW: put navigation in a separate directory, the components directory must contain only common components for the whole app
-// like buttons, form fields etc
+// like buttons, form fields etc -> DONE
 
 const Navigation = () => {
 
@@ -15,11 +15,8 @@ const Navigation = () => {
 
     return (
         <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-            headerShown: true,
-        }}>
-            <Stack.Screen name="Your Pokedex" component={Home}></Stack.Screen>
+        initialRouteName="Home">
+            <Stack.Screen name="YourPokedex" component={Home}></Stack.Screen>
             <Stack.Screen name="NextPokemons" component={NextPokemons}></Stack.Screen>
             <Stack.Screen name="PokemonsNotFound" component={PokemonsNotFound}></Stack.Screen>
             <Stack.Screen name="PokemonDetails" component={PokemonDetails}></Stack.Screen>
